45df7ab2b6d36e138f7be8a60a2e79814e231e2f
Dean L Betty <deanlloyd64@gmail.com>
2019-09-20 04:30:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :word, :matches
+  
+  def initialize(word)
+    @word = word
+    @matches = []
+    @anagrams = nil
+  end
+  
+  def match(word_list)
+    word_list.each do |possible_anagram|
+      #binding.pry
+      letter_array = possible_anagram.split("")
+      if letter_array.sort == @word.split("").sort
+        @matches << possible_anagram
+      end
+    end
+    @matches
+  end
+end
